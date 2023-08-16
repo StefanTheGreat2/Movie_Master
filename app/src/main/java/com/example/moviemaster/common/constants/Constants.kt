6d775f0e8ecaafb31a2f1b3d7422db7d1f24328a
StefanTheGreat2 <93552675+StefanTheGreat2@users.noreply.github.com>
2023-08-16 09:30:08 +0100

    Update Constants.kt

--- app/src/main/java/com/example/moviemaster/common/constants/Constants.kt
+++ app/src/main/java/com/example/moviemaster/common/constants/Constants.kt
@@ -2,5 +2,4 @@ package com.example.moviemaster.common.constants
 
 const val DB_NAME = "movie_master_db"
 const val BASE_URL = "https://api.themoviedb.org/"
-const val API_KEY = "get key from BASE_URL"
 const val BASE_POSTER_PATH = "https://image.tmdb.org/t/p/w342"
